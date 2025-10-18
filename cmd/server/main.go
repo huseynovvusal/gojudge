5cc44c0f8c7626f2a6c93910e76b714dad9635dd
Vusal Huseynov <vslh333222111@gmail.com>
2025-10-18 21:04:18 +0400

    Refactor main function to update code examples and improve error logging in RunCode execution

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,16 +6,16 @@ import (
 )
 
 func main() {
-
-	result, err := judge.RunCode("python", "print('Hello, World!')", 2, 256, 1)
-	// result, err := judge.RunCode("cpp", "#include <iostream>\nint main() { std::cout << \"Hello, C++ World!\" << std::endl; return 0; }", 2, 256, 1)
+	// Python code to run a loop 10^5 times
+	// pythonCode := "for i in range(10**7):\n\tif i % 10000 == 0: print(i)"
+	// result, err := judge.RunCode("python", pythonCode, 1, 256, 1)
+	// C code to print numbers from 0 to 10^5
+	cCode := "#include <stdio.h>\nint main() {\n    for (int i = 0; i < 100000; i++) {\n        if (i % 10000 == 0) printf(\"%d\\n\", i);\n    }\n    return 0;\n}"
+	result, err := judge.RunCode("c", cCode, 2, 256, 1)
 	if err != nil {
-		fmt.Printf("Error: %v\nOutput: %s\n", err, result.Output)
+		fmt.Printf("Error: %v", err)
 		return
-
 	}
-
 	println("Output:\n" + result.Output)
 	println("Execution Time (ms):", result.ExecutionMs)
-
 }
